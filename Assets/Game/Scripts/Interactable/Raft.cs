862fa6fd79ea88c958df6e2a7102411ea7f9aa68
Dakshvir Rehill <dakshvirr@gmail.com>
2020-05-08 16:54:28 -0400

    Downgraded unity version

--- Assets/Game/Scripts/Interactable/Raft.cs
+++ Assets/Game/Scripts/Interactable/Raft.cs
@@ -2,7 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
-public class Raft : MonoBehaviourPun
+public class Raft : MonoBehaviourPun,IPunObservable
 {
     [HideInInspector] public bool mSelected;
+
+    public void OnPhotonSerializeView(PhotonStream pStream, PhotonMessageInfo pInfo)
+    {
+        pStream.Serialize(ref mSelected);
+    }
 }
