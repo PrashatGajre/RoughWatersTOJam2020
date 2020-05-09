90fabb48d3320e9bdf853a7f241712fdee3e11cf
_Prashat <prashat@ymail.com>
2020-05-09 18:57:38 -0400

    Scene loading and events working with networking.

---
+++ Assets/Game/Scripts/Menus/SceneLoadingMenu.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SceneLoadingMenu : Menu
+{
+    [SerializeField] Text mLoadingMessageText;
+    [SerializeField] string[] mLoadingMessages;
+
+    public override void ShowMenu(string pOptions)
+    {
+        ShowMessage();
+        base.ShowMenu(pOptions);
+    }
+
+    public void ShowMessage()
+    {
+        if (mLoadingMessages.Length > 0)
+        {
+            mLoadingMessageText.text = mLoadingMessages[Random.Range(0, mLoadingMessages.Length)];
+        }
+        else
+        {
+            mLoadingMessageText.text = "Loading Resources";
+        }
+    }
+}
