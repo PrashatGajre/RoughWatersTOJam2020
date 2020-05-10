605db9cc6ff9e7854d7ce42a47686ccd7ac0ed03
Dakshvir Rehill <dakshvirr@gmail.com>
2020-05-09 22:08:05 -0400

    Added Barrier obstacle logic

---
+++ Assets/Game/Scripts/Interactable/Obstacle/BarrierObstacle.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BarrierObstacle : Obstacle
+{
+    [SerializeField] Raft.RaftType mImmuneRaft;
+    bool mActive = true;
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(!mActive)
+        {
+            return;
+        }
+        Raft aRaft = collision.GetComponent<Raft>();
+        if(aRaft.mRaftIndex == mImmuneRaft)
+        {
+            mActive = false;
+            return;
+        }
+        ObstacleBehaviour(aRaft);
+    }
+}
