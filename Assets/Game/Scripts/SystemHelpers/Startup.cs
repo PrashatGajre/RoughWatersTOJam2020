f6cbaeb4aadfacca942cecdf676c0c017ce6e3e3
Dakshvir Rehill <dakshvirr@gmail.com>
2020-05-10 18:58:50 -0400

    Added Audio

--- Assets/Game/Scripts/SystemHelpers/Startup.cs
+++ Assets/Game/Scripts/SystemHelpers/Startup.cs
@@ -45,5 +45,6 @@ public class Startup : MonoBehaviour
 		MultiSceneManager.Instance.mOnSceneLoad.RemoveListener(SceneLoadedCallback);
 		MenuManager.Instance.HideLoad();
 		MenuManager.Instance.ShowMenu(NetworkManager.Instance.mStartMenu);
+		AudioManager.Instance.PlayMusic("Music", true, 0.2f);
 	}
 }
