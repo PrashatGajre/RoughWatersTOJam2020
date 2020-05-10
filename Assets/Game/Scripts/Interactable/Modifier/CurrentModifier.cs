aae65d1af8c1040ba418be09d0291c04e283b907
Dakshvir Rehill <dakshvirr@gmail.com>
2020-05-09 21:06:31 -0400

    Added current modifier logic

---
+++ Assets/Game/Scripts/Interactable/Modifier/CurrentModifier.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CurrentModifier : MonoBehaviour
+{
+    [Range(0.0f,10f)]
+    [SerializeField] float mGravityMultiplier;
+
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        Raft aRaft = collision.GetComponent<Raft>();
+        if(aRaft != null)
+        {
+            aRaft.mGravityScaleMultiplier *= mGravityMultiplier;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        Raft aRaft = collision.GetComponent<Raft>();
+        if(aRaft != null)
+        {
+            aRaft.mGravityScaleMultiplier /= mGravityMultiplier;
+        }
+    }
+
+
+}
