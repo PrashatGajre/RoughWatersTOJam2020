43c102e9610c0d36509a9348fe5f31e83a23fbc6
Dakshvir Rehill <dakshvirr@gmail.com>
2020-05-09 21:54:01 -0400

    Added interactable items

---
+++ Assets/Game/Scripts/Interactable/Item/Item.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    public enum Type
+    {
+        Health,
+        Fatigue,
+        Score,
+        Multiplier
+    }
+
+    [SerializeField] Type mType;
+    [SerializeField] float mModifierAmount = 1.0f;
+
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        Raft aRaft = collision.GetComponent<Raft>();
+        if(aRaft != null)
+        {
+            switch(mType)
+            {
+                case Type.Fatigue:
+                    {
+                        aRaft.mFatigue += mModifierAmount;
+                        if(aRaft.mFatigue > aRaft.mMaxFatigue)
+                        {
+                            aRaft.mFatigue = aRaft.mMaxFatigue;
+                        }
+                        break;
+                    }
+                case Type.Health:
+                    {
+                        aRaft.mHealth += mModifierAmount;
+                        if(aRaft.mHealth > aRaft.mMaxHealth)
+                        {
+                            aRaft.mHealth = aRaft.mMaxHealth;
+                        }
+                        break;
+                    }
+                case Type.Multiplier:
+                    {
+                        DataHandler.Instance.mScoreMultiplier *= mModifierAmount;
+                        break;
+                    }
+                case Type.Score:
+                    {
+                        DataHandler.Instance.mCurrentScore += DataHandler.Instance.mScoreMultiplier * mModifierAmount;
+                        break;
+                    }
+            }
+            Destroy(gameObject);
+        }
+    }
+
+
+}
--- Assets/Game/Scripts/Managers/DataHandler.cs
+++ Assets/Game/Scripts/Managers/DataHandler.cs
@@ -5,11 +5,30 @@ using UnityEngine;
 public class DataHandler : Singleton<DataHandler>
 {
     public Raft[] mActiveRafts;
+    [SerializeField] Transform mRaftTargetGroup;
+    [SerializeField] float mLevelTraversalScore = 10.0f;
+    [HideInInspector] public float mCurrentScore = 0;
+    [HideInInspector] public float mScoreMultiplier = 1.0f;
+    float mCurrentMaxDistance = 0.0f;
+    Vector3 mStartPosition = Vector3.zero;
     void Start()
     {
         for(int aI = 0; aI < mActiveRafts.Length;aI ++)
         {
             mActiveRafts[aI].mRaftIndex = (Raft.RaftType)aI;
         }
+        mStartPosition = mRaftTargetGroup.position;
     }
+
+    void Update()
+    {
+        float aDistance = Vector3.Distance(mRaftTargetGroup.position, mStartPosition);
+        if(mCurrentMaxDistance < aDistance)
+        {
+            mCurrentScore += mScoreMultiplier * mLevelTraversalScore * (aDistance - mCurrentMaxDistance);
+            mCurrentMaxDistance = aDistance;
+        }
+    }
+
+
 }
